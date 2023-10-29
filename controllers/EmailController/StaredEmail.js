dc0783d90d127b5cf2e1f5895fe10b9d8fdb6932
Sathishramesh1 <sathishrameshkec@gmail.com>
2023-10-29 22:27:06 +0530

    error-render-starredmsg

--- controllers/EmailController/StaredEmail.js
+++ controllers/EmailController/StaredEmail.js
@@ -4,7 +4,7 @@ import {Email} from '../../models/Email.js'
 const StaredEmail=async(req,res)=>{
     try {
      const {messageid}=req.params;
-     const userEmail= await Email.findOne({user:req.user._id}).populate('inbox');
+     const userEmail= await Email.findOne({user:req.user._id});
      
      if(userEmail){
        let changeValue=userEmail.inbox.find((message)=>message._id==messageid).starred ; 
