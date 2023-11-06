c2c815c03afb6b95103244ea22f3e4ebd5a96766
Sathishramesh1 <sathishrameshkec@gmail.com>
2023-11-06 22:35:52 +0530

    update-delete

--- controllers/EmailController/DeleteMail.js
+++ controllers/EmailController/DeleteMail.js
@@ -12,17 +12,46 @@ const DeleteEmail=async(req,res)=>{
        });
       
       //checking message is in which array
-        if(checkEmail.inbox.id(messageid) || checkEmail.send.id(messageid) || checkEmail.draft.id(messageid) ){
-    const deltedMessage= checkEmail.inbox.id(messageid) || checkEmail.send.id(messageid) || checkEmail.draft.id(messageid);
-        checkEmail.trash.push(deltedMessage);
+    //     if(checkEmail.inbox.id(messageid) || checkEmail.send.id(messageid) || checkEmail.draft.id(messageid) ){
+    //  const deltedMessage= checkEmail.inbox.id(messageid) || checkEmail.send.id(messageid) || checkEmail.draft.id(messageid);
+    //   checkEmail.trash.push(deltedMessage);
           
 
-        checkEmail.inbox.pull(messageid) || checkEmail.send.pull(messageid) || checkEmail.draft.pull(messageid);// Remove the object from the array
-        await checkEmail.save(); // Save the updated document
-        res.status(200).json({ message: 'message deleted successfully' });
-          }
+    //     checkEmail.inbox.pull(messageid) || checkEmail.send.pull(messageid) || checkEmail.draft.pull(messageid);// Remove the object from the array
+    //     await checkEmail.save(); // Save the updated document
+    //     res.status(200).json({ message: 'message deleted successfully' });
+    //       }
           
-          else {
+
+    if (checkEmail) {
+      let deletedMessage;
+      
+      // Check and delete from inbox array
+      deletedMessage = checkEmail.inbox.find(message => message._id == messageid);
+      if (deletedMessage) {
+        checkEmail.inbox.pull(messageid);
+      }
+
+      // Check and delete from send array
+      deletedMessage = checkEmail.send.find(message => message._id == messageid);
+      if (deletedMessage) {
+        checkEmail.send.pull(messageid);
+      }
+
+      // Check and delete from draft array
+      deletedMessage = checkEmail.draft.find(message => message._id == messageid);
+      if (deletedMessage) {
+        checkEmail.draft.pull(messageid);
+      }
+
+      // Move the deleted message to the trash array
+      if (deletedMessage) {
+        checkEmail.trash.push(deletedMessage);
+        await checkEmail.save(); // Save the updated document
+        res.status(200).json({ message: 'Message deleted successfully' });
+      } 
+    }
+       else {
         res.status(404).json({ message: 'Document not found' });
       }
           
