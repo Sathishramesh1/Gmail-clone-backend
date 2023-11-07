d0ebba1ebf075450279a9ea31b8f8b23aa35875b
Sathishramesh1 <sathishrameshkec@gmail.com>
2023-11-07 17:38:58 +0530

    update-getdraft

--- controllers/EmailController/GetDraft.js
+++ controllers/EmailController/GetDraft.js
@@ -4,13 +4,14 @@ import {Email} from '../../models/Email.js'
 const GetDraft=async(req,res)=>{
     try {
         
-const userEmail= await Email.findOne({user:req.user_id}).populate('draft'); 
-       if(userEmail){
+const userEmail= await Email.findOne({user:req.user._id});
+console.log(userEmail); 
+    if(userEmail){
           const DraftMail=userEmail.draft;
           res.status(200).json({
               DraftMail,
               message:"Message retrived successfully"
-          }) 
+          }); 
        }else{
            res.status(404).send("problem in retriving draft mails");
        }
